b5115a610bbdbd6a3839c3fb4ec59497901d670e
Eric Santana <ericsantana@Erics-Mac-mini.local>
2021-11-26 09:07:10 -0800

    contact head tag

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import ContactForm from "../components/Contact/contactForm";
 import NavBar from "../components/Navbar/navbar";
 import Footer from "../components/Footer/footer";
@@ -6,6 +7,11 @@ import styles from "../styles/Contact.module.css";
 export default function Contact() {
   return (
     <div>
+      <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Arrive on time and in style." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <NavBar contact={true} />
       <div className={styles["contact-form"]}>
         <ContactForm />
